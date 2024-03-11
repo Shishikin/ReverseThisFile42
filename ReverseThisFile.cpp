032aafa3227dab35985e0d7962eb6c2877ea5c3f
Shishikin <mvg1313@mail.ru>
2024-03-11 22:57:01 +0300

    win

--- ReverseThisFile.cpp
+++ ReverseThisFile.cpp
@@ -1,46 +1,59 @@
-﻿#include <iostream> 
+﻿#include <iostream>
 #include <fstream>
-
 #ifdef _WIN32
 #include <windows.h>
 #endif
-
-
 int main()
 {
 #ifdef _WIN32
-    setlocale(LC_ALL, "ru");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-#endif
-
     std::ifstream in;
-    in.open("ReverseThisFile.cpp");
-    if (!in.is_open())
+    in.open(__FILE__);    
+    if (!in.is_open() && in.fail())
     {
         std::cout << "error";
-    }
+    } 
 
-    char test = '0';
-    int count = 0;
-    
-    while (!in.eof())
-    {
-        in.get(test);
-        count++;
-    }
-    in.close();
-    in.open("ReverseThisFile.cpp");
-    if (!in.is_open())
-    {
-        std::cout << "error";
-    }///uyfuy
-    for (int i = count + 42; i >= 3; i --)
+    char ch;    
+    in.seekg(0, std::ios::end);
+    int flag = 0;
+    int position = in.tellg();
+    for (int i = position - 1; i >= 3; i--)
     {
         in.seekg(i, std::ios::beg);
-        in.get(test);
-        std::cout << test;
-    }                       
+        in.get(ch);
+        if (ch != '\n')
+        {
+            std::cout << ch;
+        }  
+        else
+        {
+            if (flag % 2 == 0)
+            {
+                std::cout << ch;
+            }
+            flag++;
+        }
+    }
     in.close();
+#else
+    std::ifstream in;
+    in.open(__FILE__);
+    if (!in.is_open() && in.fail())
+    {
+        std::cout << "error";
+    }
+    char ch;
+    in.seekg(0, std::ios::end);
+    int position = in.tellg();
+    for (int i = position - 1; i >= 3; i--)
+    {
+        in.seekg(i, std::ios::beg);
+        in.get(ch);
+        std::cout << ch;
+    }
+    in.close();
+#endif    
     return 0;
 }
